cb010edf736994503ba6dc999ffbdfefa0a64dd1
chocotime <duckhoim@daum.net>
2017-03-18 20:23:41 +0900

    설탕배달
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초	128 MB	10458	2621	2205	29.013%
    문제
    상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
    
    상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
    
    상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
    
    * 첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
    * 상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.

---
+++ 2839.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+    int N;
+    scanf("%d", &N);
+    if(3<=N && N<=5000) {
+        int rem = N%5;
+        
+        while(rem%3!=0 && rem<=N) {
+            rem+=5;
+        }
+        
+        if(rem>N) 
+          printf("-1\n");
+        else if(rem==N)
+          printf("%d\n", rem/3);
+        else
+          printf("%d\n", (N-rem)/5+rem/3);
+    }
+    return 0;
+}

ccddb0ca276e0e01247504f1e0eaeee569a6b05b
chocotime <duckhoim@daum.net>
2017-03-19 14:42:47 +0900

    for 1-8..

--- 11718.c
+++ 11718.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 int main(void) {
 	int putting;
-	int mkline = 0;
+	int linecheck = 0, mkline=0;
+	int n=0;
 	while ( (putting=getchar()) != EOF ) {
-		if(putting=='\n') mkline++;
+		n++;
+		printf("%d", n);
+		if(putting=='\n') {
+			mkline++;
+			linecheck++;
+			n=0;
+		}
 		else mkline=0;
+		if(linecheck>100) {
+			break;
+		}
 		if(mkline >= 2) break;
+		if(n>100) break;
 		putchar(putting);
 	}
 	return 0;

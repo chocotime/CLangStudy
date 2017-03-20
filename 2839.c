ae53082613bbfb44c1e8a9fabb464522ac0a5f1b
chocotime <duckhoim@daum.net>
2017-03-20 10:01:02 +0900

    code fixed

--- 2839.c
+++ 2839.c
@@ -4,19 +4,17 @@ int main(void) {
     scanf("%d", &N);
     if(3<=N && N<=5000) {
         int rem = N%5;
-        
+        if(rem==0) {
+          printf("%d\n", N/5);
+          return 0;
+        }
         while(rem%3!=0 && rem<=N) {
             rem+=5;
         }
-        
-        if(rem>N) 
+        if(rem>N)
           printf("-1\n");
-        else if(rem==N) {
-          if(rem%3!=0) 
-              printf("-1\n");
-          else 
-              printf("%d\n", rem/3);
-        }
+        else if(rem==N)
+          printf("%d\n", rem/3);
         else
           printf("%d\n", (N-rem)/5+rem/3);
     }
